ed33165c445adb77197d8a3dc0bfb83b4c034237
Miroslav Bondik <benya.senya.16@gmail.com>
2023-04-27 18:54:01 +0300

    Add files via upload

--- test-ligamedia/script.js
+++ test-ligamedia/script.js
@@ -7,11 +7,14 @@ const mainContainer = document.querySelector('.main__container')
 const copyIcon = document.querySelector('img[alt="copy-icon"]');
 const description = document.querySelector('.overview-description');
 const overlayCopy = document.querySelector('.overlay-copy')
+const html = document.querySelector('html')
+const body = document.querySelector('body')
+
 
 
 
 // Change view hamburger on mobile or desktop (like in Figma)
-if (window.screen.width >= 360 && window.screen.width <= 540) {
+if (window.screen.width >= 320 && window.screen.width <= 540) {
     imgHamburger.setAttribute('src', "icons/hamburger-mobile.svg")
 }
 
@@ -27,14 +30,20 @@ const openMenu = (event) => {
         mainNav.classList.add('hamburger-active')
         headerNav.classList.add('header-nav-active')
         mainContainer.classList.add('header-nav-active')
+        html.classList.add('scroll-off')
+        body.classList.add('scroll-off')
+
     } else {
         mainNav.classList.add('none')
         divImgHamburger.classList.remove('hamburger-active')
         mainNav.classList.remove('hamburger-active')
         headerNav.classList.remove('header-nav-active')
         mainContainer.classList.remove('header-nav-active')
+        html.classList.remove('scroll-off')
+        body.classList.remove('scroll-off')
     }
 }
+
 
 
 // Open subtext on nav menu
